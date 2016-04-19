43d19a87576146ead7b9aeda716782118ca19332
root <root@owncloud-srv.epfl.ch>
2016-04-19 12:34:06 +0200

    Code organised

--- mp3-dl.sh
+++ mp3-dl.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
+# Test command synthax 
 if [ "$1" = "" ]; then
  echo "./mp3-dl.sh <YouTube URL>"
  exit
 fi
 
+# Define variables
 URL=$1
-DD=/home/admin-srv/tmp/tmp.mp3
+DD=/home/admin-srv/tmp/
 CD=/home/admin-srv/owncloud_music/data/loichu/files/Music
 
+# Extract mp3 from YouTube
 youtube-dl -o $DD --extract-audio --audio-format mp3 $URL
 
-# Ici fingerpring
+# Find fingerprint
 fpcalc $DD > /home/admin-srv/tmp/fp.txt
 . fp.txt
 
-# get audio description json
-wget http://api.acoustid.org/v2/lookup? & client=ezZaIw4Wm6 & duration=$DURATION & fingerprint=$FINGERPRINT & meta=recordings+releases > /home/admin-srv/tmp/metadatas.json
+# Test variables (from fp.txt)
+echo "duration="$DURATION
+echo "fingerprint="$FINGERPRINT
 
+# get audio description json
+curl -F "client=ezZaIw4Wm6;duration=$DURATION;fingerprint=$FINGERPRINT;meta=recordings+releases" http://api.acoustid.org/v2/lookup? > /home/admin-srv/tmp/metadatas.json
+
+# test json result
+echo /home/admin-srv/tmp/metadatas.json
+
+# return json variables
+
+# set metadatas in mp3 file
+
+# move file in cloud (/home/admin-srv/Music/Artist/Album/song.mp3)
+
+# delete cache
 rm /home/admin-srv/tmp/*
-# rename and  move current.mp3 /home/dossierparageOwnCloud/Auteur/Album/titre.mp3
